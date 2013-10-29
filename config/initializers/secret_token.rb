386b4f63d5e81832f1b00a49ed7d27cfa81d2b74
Eric Boersma <eric.boersma@gmail.com>
2013-10-29 19:28:52 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jabberbox::Application.config.secret_token = '2e0e8fdce7f5cb9e0f45e9056d0f9a58c9d67910d670a1ffc8d158a2409735d02cf3e27b5fb4b285df62496d54ea1cc8a31a7b9f1e249451c0f827db68574dd9'
